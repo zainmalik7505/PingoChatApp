3bfa4f4863e8689c9315af66d4f20067dacf7129
Zain Malik <xaineil121@gmail.com>
2024-08-09 14:17:33 +0500

    Initial Commit

---
+++ Pingo/PingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingoApp.swift
+//  Pingo
+//
+//  Created by Zain Malik on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
